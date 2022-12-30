a8747a34aa6042428fbeea741beb399b03438204
fk-faruk <faruktaiwo@gmail.com>
2022-12-30 19:50:38 +0100

    vercel settings added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 // const e js = require('ejs')
 
 app.set("view engine", "ejs");
